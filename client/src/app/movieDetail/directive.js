c66918e59742e6987a6a2b2a9a73ce1c9788fcdf
Gonzales Michael <michael.gonzales@supdeweb-paris.com>
2016-03-18 13:38:41 +0100

    prod companies and countries add movie detail

--- client/src/app/movieDetail/directive.js
+++ client/src/app/movieDetail/directive.js
@@ -17,9 +17,19 @@
           vm.movie = response.data;
           vm.movie.vote = (vm.movie.vote_average*10);
           vm.movie.genres_name = [];
+          vm.movie.production_companies_name = [];
+          vm.movie.production_countries_name = [];
           for (var i = 0; i <= vm.movie.genres.length-1; i++) {
             vm.movie.genres_name.push(vm.movie.genres[i].name);
             vm.movie.genres_name.sort();
+          }
+          for (var i = 0; i <= vm.movie.production_companies.length-1; i++) {
+            vm.movie.production_companies_name.push(vm.movie.production_companies[i].name);
+            vm.movie.production_companies_name.sort();
+          }
+          for (var i = 0; i <= vm.movie.production_countries.length-1; i++) {
+            vm.movie.production_countries_name.push(vm.movie.production_countries[i].name);
+            vm.movie.production_countries_name.sort();
           }
         });
       },
